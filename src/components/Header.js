782d43821da618b71005c2a492e53f4a903aac2c
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2021-10-12 21:32:17 -0700

    created header

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='container px-6 py-4'>
+      <div className='md:flex md:items-center md:justify-between'>
+        <div className='flex items-center justify-between'>
+          <div>
+            <img 
+              className='h-8'
+              src='https://upload.wikimedia.org/wikipedia/commons/6/67/NewNetflixLogo.png' 
+              alt=''
+            />
+          </div>
+
+          {/* <!-- Mobile menu button --> */}
+          <div class="flex md:hidden">
+              <button type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                  <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                      <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                  </svg>
+              </button>
+          </div>       
+        </div>
+
+        {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
+        <div className='w-full md:flex md:items-center md:justify-between'>
+          <div className='flex flex-col px-2 py-3 -mx-4 md:flex-row md:mx-0 md:py-0'>
+            <a href="#" class="px-2 py-1 text-sm font-medium text-gray-50 transition-colors duration-200 hover:text-gray-400 md:mx-2">Home</a>
+            <a href="#" class="px-2 py-1 text-sm font-medium text-gray-50 transition-colors duration-200 hover:text-gray-400 md:mx-2">TV Shows</a>
+            <a href="#" class="px-2 py-1 text-sm font-medium text-gray-50 transition-colors duration-200 hover:text-gray-400 md:mx-2">Movies</a>
+            <a href="#" class="px-2 py-1 text-sm font-medium text-gray-50 transition-colors duration-200 hover:text-gray-400 md:mx-2">New & Popular</a>
+            <a href="#" class="px-2 py-1 text-sm font-medium text-gray-50 transition-colors duration-200 hover:text-gray-400 md:mx-2">My List</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
