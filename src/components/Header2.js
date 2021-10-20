0d61ff98b73e026e53bfc635a10be187079be746
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2021-10-20 15:52:36 -0700

    added search and bell icon to header, added netflix avatar logo

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -4,8 +4,8 @@ import { ChevronDownIcon } from '@heroicons/react/solid'
 
 const Header2 = () => {
   return (
-    <div className='px-8 py-4 mx-auto'>
-      <div className='md:flex md:items-center md:justify-between'>
+    <div className='px-8 py-2 w-full mx-auto lg:py-6'>
+      <div className='flex items-center justify-between'>
         <div className='flex items-center'>
             <img 
               className='h-8'
@@ -32,7 +32,7 @@ const Header2 = () => {
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             >
-              <Menu.Items className='absolute flex flex-col items-center justify-between left-10 w-64 h-60 mt-12 p-2 bg-gray-400 origin-top focus:outline-none'>
+              <Menu.Items className='absolute flex flex-col items-center justify-between left-10 w-64 h-60 mt-12 p-2 bg-gray-400 rounded-sm origin-top focus:outline-none'>
                 <Menu.Item className="py-1 text-sm font-medium text-gray-50 transition-colors duration-200 hover:text-gray-400 md:mx-2">
                   <a href="#">Home</a>
                 </Menu.Item>
@@ -64,24 +64,26 @@ const Header2 = () => {
           </div>
         </div>
 
-        <div>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-          </svg>
-        </div>
+        <div className='flex items-center justify-end w-1/3'>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+            </svg>
+          </div>
 
-        <div>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-          </svg>
-        </div>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+            </svg>
+          </div>
 
-        <div>
-          <img 
-              className='h-10 w-12 rounded'
-              src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
-              alt='' 
-            />
+          <div>
+            <img 
+                className='h-8 rounded'
+                src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+                alt='' 
+              />
+          </div>
         </div>
       </div>
     </div>
